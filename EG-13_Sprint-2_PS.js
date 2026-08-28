1192986eee5cfa1aba7129714cb771e9d544a16b
Nayem Ahmed <nayemk0087@gmail.com>
2026-08-28 19:52:35 +0600

    solution: solve string reversal problem

---
+++ EG-13_Sprint-2_PS.js
@@ -0,0 +1,23 @@
+
+// Problem - 1 
+
+/**
+ * @param {string} str
+ * @return {string}
+ */
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// 5ti Example run kore dekhar jonno
+console.log(reverseString("hello"));                  // Output: olleh
+console.log(reverseString("JavaScript"));             // Output: tpircSavaJ
+console.log(reverseString("Madam In Eden Im Adam"));  // Output: madam mI nedE nI madaM
+console.log(reverseString("12345!@#$"));              // Output: $#@!54321
+console.log(reverseString("A"));                      // Output: A
+
+
+
+
+
+
